2a23a56fd602a746a70a90b343f6243e380f18d5
Carter Adams <carteradams@protonmail.com>
2019-09-29 15:24:49 -0400

    further simplifications, add c-r-e default page back, test of fs and electron import from

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
-import Electron from 'electron';
+import logo from './logo.svg';
+import './App.css';
+
 import fs from 'fs';
+import electron from 'electron';
+
+console.log(fs);
+console.log(electron);
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
